faf6c6068a39a4ed798f41b02e829807cf27d12a
PARVATHIL3 <126845688+PARVATHIL3@users.noreply.github.com>
2023-05-24 16:05:35 +0530

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -1,14 +1,19 @@
-broker = "34.100.196.230";
+import paho.mqtt.client as paho
+import pymysql
+global mqttclient;
+global broker;
+global port;
+broker = "localhost";
 port = 1883;
 client_uniq = "pubclient_123"
 mqttclient = paho.Client(client_uniq, True) 
-def storeData(mesg):
+def storeData(mesg,t):
  #Create a connection to MySQL Database 
  conn =pymysql.connect(database="Iotdatap",user="user2",password="Pass1234",host="localhost")
  #Create a MySQL Cursor to that executes the SQLs
  cur=conn.cursor()
  #Create a dictonary containing the fields, name, age and place
- data={'topic':'IoTtest','data':mesg}
+ data={'topic':t,'data':mesg}
  #Execute the SQL to write data to the database
  cur.execute("INSERT INTO Sensordata(Topic , Sensorvalue)VALUES(%(topic)s,%(data)s);",data)
  print("Data added")
@@ -21,17 +26,18 @@ def storeData(mesg):
 def test(client, userdata, message):
  print("client:"+ str(client))
  print("userdata:"+ str(userdata))
- print("message:"+ str(message.payload))
+ print("message:"+ str(message.payload.decode()))
+ storeData(message.payload.decode(),message.topic)
 def _on_message(client, userdata, msg):
 # print("Received: Topic: %s Body: %s", msg.topic, msg.payload)
-print(msg.topic+" "+str(msg.payload))
+ print(msg.topic+" "+str(msg.payload))
 #Subscribed Topics 
 def _on_connect(mqttclient, userdata, flags, rc):
 # print("New Client: "+str(mqttclient)+ " connected")
 # print(rc)
-mqttclient.subscribe("IOT/#", qos=0)
+ mqttclient.subscribe("IOT/#", qos=0)
  
-mqttclient.message_callback_add("IoTtest", test)
+mqttclient.message_callback_add("IOT/test", test)
 mqttclient.connect(broker, port, keepalive=1, bind_address="")
  
 mqttclient.on_connect = _on_connect
