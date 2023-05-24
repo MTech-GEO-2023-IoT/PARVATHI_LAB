bc68b754b28e60bf0adb1c1bc4eb711885b2d4cc
PARVATHIL3 <126845688+PARVATHIL3@users.noreply.github.com>
2023-05-24 10:09:47 +0530

    Update mysql.py

--- mysql.py
+++ mysql.py
@@ -5,7 +5,7 @@ global broker;
 global port;
 
 
-broker = "0.0.0.0";
+broker = "34.100.196.230";
 port = 1883;
 
 client_uniq = "pubclient_123"
@@ -14,11 +14,11 @@ mqttclient = paho.Client(client_uniq, True)
 
 def storeData(mesg):
   #Create a connection to MySQL Database 
-  conn =pymysql.connect(database="rohit",user="rohit",password="pass",host="localhost")
+  conn =pymysql.connect(database="Iotdatap",user="user2",password="Pass1234",host="localhost")
   #Create a MySQL Cursor to that executes the SQLs
   cur=conn.cursor()
   #Create a dictonary containing the fields, name, age and place
-  data={'topic':'IOT/test','data':mesg}
+  data={'topic':'IoTtest','data':mesg}
   #Execute the SQL to write data to the database
   cur.execute("INSERT INTO mqttdata(topic , data)VALUES(%(topic)s,%(data)s);",data)
   print("Data added")
@@ -44,7 +44,7 @@ def _on_connect(mqttclient, userdata, flags, rc):
 # 	print(rc)
 	mqttclient.subscribe("IOT/#", qos=0)	
   
-mqttclient.message_callback_add("IOT/test", test)
+mqttclient.message_callback_add("Iotdatap", test)
 
 mqttclient.connect(broker, port, keepalive=1, bind_address="")
   
