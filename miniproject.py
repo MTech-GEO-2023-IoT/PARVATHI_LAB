3a328137ec0ea39b73268dab31054bc778145372
PARVATHIL3 <126845688+PARVATHIL3@users.noreply.github.com>
2023-06-14 23:42:39 +0530

    Update miniproject.py

--- miniproject.py
+++ miniproject.py
@@ -1,10 +1,11 @@
+
 from bluetooth import *
 
 # Bluetooth device address of the terminal
 target_address = '7c:d6:61:1d:39:c6'  # Replace with the target device's address
 
 # UUID of the RFCOMM service
-service_uuid = '00001101-0000-1000-8000-00805F9B34FB'
+service_uuid = '00001801-0000-1000-8000-00805f9b34fb'
 
 # Create a Bluetooth socket
 sock = BluetoothSocket(RFCOMM)
